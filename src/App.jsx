feecbebf00a60c74981741da786c61592d4ec504
Larysa Rud <larpavrud@gmail.com>
2025-06-21 11:24:30 -0400

    Create an About page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
+import About from './pages/About';
 import './App.css'
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />}/>
+          <Route path="about" element={<About />} />
       </Route>
     </Routes>
   );

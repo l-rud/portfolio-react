cb03812de44a2f34a5b8f9cd370e1de74444b6f3
Larysa Rud <larpavrud@gmail.com>
2025-06-21 11:31:46 -0400

    Center the text in the About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div>
+        <div className="about-container">
       <h3>
       <p>
         Hi, I’m Larysa, an ASTQB-certified professional and full-stack web development enthusiast. I’ve worked with tools like Jira, TestRail, Postman, VS Code, Git, and GitHub. I enjoy writing code in JavaScript, HTML, CSS, and working with SQL.

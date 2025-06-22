c29dc5db922f12c4a55f6def515586222b509df9
Larysa Rud <larpavrud@gmail.com>
2025-06-22 08:31:27 -0400

    Remove duplicated buttons from Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,15 +5,6 @@ export default function Home() {
   return (
     <div className="home-container">
       <h1>Welcome to my Portfolio!</h1>
-
-      <div className="button-group">
-        <Link to="/about" className="pop-button">
-          About Me
-        </Link>
-        <Link to="/projects" className="pop-button">
-          View Projects
-        </Link>
-      </div>
     </div>
   );
 }

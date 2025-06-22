2a5a02cacd7703fca21089091a4700002923d1ef
Larysa Rud <larpavrud@gmail.com>
2025-06-22 07:03:26 -0400

    Create projects page and projects route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
+import Projects from './pages/Projects';
 import './App.css'
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />}/>
           <Route path="about" element={<About />} />
+          <Route path="projects" element={<Projects />} />
       </Route>
     </Routes>
   );
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Projects() {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <p>Project 1</p>
+       <p>Project 2</p>
+    </div>
+  );
+}

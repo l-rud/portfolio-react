3e26261032972ced044914f24a4d363a864a9e33
Larysa Rud <larpavrud@gmail.com>
2025-06-22 13:20:54 -0400

    Not-found page styling

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,13 @@
 import PageWrapper from '../components/PageWrapper';
+import './NotFound.css';
+
 export default function NotFound() {
   return (
     <PageWrapper>
-    <div style={{ textAlign: 'center' }}>
-      <h1>404 - Page Not Found</h1>
-      <p>Oops! The page you're looking for doesn't exist.</p>
-    </div>
+      <div className="not-found-page">
+        <h1>404 - Page Not Found</h1>
+        <p>Oops! The page you're looking for doesn't exist.</p>
+      </div>
     </PageWrapper>
   );
 }

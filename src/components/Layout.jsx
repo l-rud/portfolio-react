f0dc0c55c6acc910ea3008440cc200a63bd0652e
Larysa Rud <larpavrud@gmail.com>
2025-06-22 08:27:59 -0400

    Put the navigation buttons at the bottom

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,12 +2,16 @@ import { Outlet, Link } from 'react-router-dom';
 
 export default function Layout() {
   return (
-    <div className="popup">
-      <nav>
-        {/* <Link to="/">Home</Link> */}
-      </nav>
+    <div className="popup layout-container">
+      <div className="content">
+        <Outlet />
+      </div>
 
-      <Outlet />
+      <nav>
+        <Link to="/" className="pop-button">Home</Link>
+        <Link to="/about" className="pop-button">About</Link>
+        <Link to="/projects" className="pop-button">Projects</Link>
+      </nav>
     </div>
   );
 }

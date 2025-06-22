07a1cc8c002abea0bfb2883a5b26d6c8b98f8616
Larysa Rud <larpavrud@gmail.com>
2025-06-22 07:52:27 -0400

    Update About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,15 +3,14 @@ import React from 'react';
 export default function About() {
   return (
         <div className="about-container">
-      <h3>
+          <h1>About Me</h1>
       <p>
         Hi, I’m Larysa, an ASTQB-certified professional and full-stack web development enthusiast. I’ve worked with tools like Jira, TestRail, Postman, VS Code, Git, and GitHub. I enjoy writing code in JavaScript, HTML, CSS, and working with SQL.
       </p>
       <p>
         These days, I'm diving into full-stack projects using the MERN stack and always looking for new things to explore.
       </p>
-      <p>Thanks for stopping by — feel free to take a look around!</p>
-      </h3>
+      <p>Thanks for stopping by — feel free to take a look around!</p>   
     </div>
   );
 }
